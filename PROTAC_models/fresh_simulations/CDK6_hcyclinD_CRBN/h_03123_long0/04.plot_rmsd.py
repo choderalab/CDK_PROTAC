dcff42b4ca0db14a2d4e9168681ccc2a581d9e94
Jiaye Guo <jiaye.guo@choderalab.org>
2020-12-10 14:30:11 -0500

    add more scripts for analysis

---
+++ PROTAC_models/fresh_simulations/CDK6_hcyclinD_CRBN/h_03123_long0/04.plot_rmsd.py
@@ -0,0 +1,57 @@
+from netCDF4 import Dataset
+import mdtraj as md
+from simtk.openmm.app import PDBFile
+import numpy as np
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+pdb = PDBFile('03123_CDK6_holo_equi.pdb')
+topology = md.Topology.from_openmm(pdb.topology)
+ref = md.load_pdb('03123_CDK6_holo_equi.pdb')
+solute_ref = ref.remove_solvent()
+t = md.load('traj.nc',top=topology)
+#t = t.image_molecules(anchor_molecules=solute_ref.topology.find_molecules()[0:1], inplace=True)
+#print('done imaging molecules')
+table, bonds = topology.to_dataframe()
+atoms = table.values
+
+# find the atom indices of different proteins in the complex
+CDK6 = list()
+CRBN = list()
+LIG = list()
+
+for i in range(len(atoms)):
+    # CDK6
+    if atoms[i][5] == 0 and atoms[i][1] in ['C', 'O', 'N', 'CA']:
+        CDK6.append(i)
+    # CRBN
+    if (atoms[i][5] == 2 and atoms[i][1] in ['C', 'O', 'N', 'CA']) or (atoms[i][5] == 3 and atoms[i][1] in ['C', 'O', 'N', 'CA']):
+        CRBN.append(i)
+    # ligand
+    if atoms[i][5] == 4 and atoms[i][4] == 'LIG':
+        LIG.append(i)
+CDK6_rmsd = md.rmsd(t, t[0], frame=0, atom_indices=np.array(CDK6))
+CRBN_rmsd = md.rmsd(t, t[0], frame=0, atom_indices=np.array(CRBN))
+LIG_rmsd = md.rmsd(t, t[0], frame=0, atom_indices=np.array(LIG))
+# plot the RMSD of different proteins
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.gridspec import GridSpec
+gs = GridSpec(1, 4)
+ax1 = plt.subplot(gs[0, 0:3])
+ax2 = plt.subplot(gs[0, 3:], sharey=ax1)
+plt.setp(ax2.get_yticklabels(), visible=False)
+x_axis = list(range(len(CDK6_rmsd)))
+ax1.scatter(x_axis, LIG_rmsd, color='#32cd32', s=5)
+ax1.scatter(x_axis, CRBN_rmsd, color='#9DA1E7', s=5)
+ax1.scatter(x_axis, CDK6_rmsd, color='#00ced1', s=5, alpha=0.5)
+ax2 = sns.distplot(LIG_rmsd, hist=False, kde=True, kde_kws={'shade': True, 'linewidth': 0.5}, vertical=True, color='#32cd32')
+ax2 = sns.distplot(CRBN_rmsd, hist=False, kde=True, kde_kws={'shade': True, 'linewidth': 0.5}, vertical=True, color='#9DA1E7')
+ax2 = sns.distplot(CDK6_rmsd, hist=False, kde=True, kde_kws={'shade': True, 'linewidth': 0.5}, vertical=True, color='#00ced1')
+#ax2.hist(np.array(LIG_rmsd), bins=50, orientation='horizontal', color='#32cd32', linewidth=0.1, align='left', alpha=0.5, edgecolor='black')
+#ax2.hist(np.array(CRBN_rmsd), bins=50, orientation='horizontal', color='#9DA1E7', linewidth=0.1, align='left', alpha=0.5, edgecolor='black')
+#ax2.hist(np.array(CDK6_rmsd), bins=50, orientation='horizontal', color='#bbedec', linewidth=0.1, align='left', edgecolor='black')
+plt.ylim(0.0,2.0)
+plt.show()
---
+++ PROTAC_models/fresh_simulations/CDK6_hcyclinD_CRBN/h_03123_long0/05.plot_key_interact.py
@@ -0,0 +1,71 @@
+import mdtraj as md
+import numpy as np
+#from math import cos
+from simtk.openmm.app import PDBFile
+from tqdm import tqdm
+
+# read in the trajectory
+pdb = PDBFile('03123_CDK6_holo_equi.pdb')
+topology = md.Topology.from_openmm(pdb.topology)
+ref = md.load_pdb('03123_CDK6_holo_equi.pdb')
+solute_ref = ref.remove_solvent()
+t = md.load('traj.nc',top=topology)
+t = t.image_molecules(anchor_molecules=solute_ref.topology.find_molecules()[0:1], inplace=True)
+print('finish imaging molecules')
+equi = 300 # the structure seems stable after this frame
+length = len(t)
+print(f'length of the simulation: {length}.')
+
+# hard code the residue indices of CDK6 and CRBN (1-based) 
+CDK6 = list(range(1,294))
+CRBN = list(range(541,922)) 
+key_inter = dict()
+lig_inter = dict()
+
+for i in tqdm(range(length)):
+    hbonds = md.baker_hubbard(t[i])
+    label = lambda hbond : '%s -- %s' % (t.topology.atom(hbond[0]), t.topology.atom(hbond[2]))
+    for hbond in hbonds:
+        if (str(t.topology.atom(hbond[0]))[4] == '-' and int(str(t.topology.atom(hbond[0]))[3]) in CDK6) or (str(t.topology.atom(hbond[0]))[5] == '-' and int(str(t.topology.atom(hbond[0]))[3:5]) in CDK6) or (str(t.topology.atom(hbond[0]))[6] == '-' and int(str(t.topology.atom(hbond[0]))[3:6]) in CDK6): # 1-digit CDK6 or 2-digit or 3-digit
+            if len(str(t.topology.atom(hbond[2]))) > 6:
+                if str(t.topology.atom(hbond[2]))[6] == '-' and int(str(t.topology.atom(hbond[2]))[3:6]) in CRBN: # CRBN can only be 3-digit
+                    if label(hbond) not in key_inter.keys():
+                        key_inter[label(hbond)] = [0] * length # initialize list for one h-bond
+                    key_inter[label(hbond)][i] = 1
+        if 'LIG' in str(t.topology.atom(hbond[2])):
+            if label(hbond) not in lig_inter.keys():
+                lig_inter[label(hbond)] = [0] * length # initialize list for one h-bond
+            lig_inter[label(hbond)][i] = 1
+
+# plot the h-bond presence
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.gridspec import GridSpec
+
+# set up subplots
+ax1=plt.subplot(2, 1, 1)
+ax2=plt.subplot(2, 1, 2)
+count1 = 1
+count2 = 1
+print("threshold: ", int((length-equi)/4))
+for label in key_inter.keys():
+    if sum(key_inter[label][equi+1:]) > int((length-equi)/4):
+        ax1.scatter(list(range(equi+2, length+1)), np.array(key_inter[label][equi+1:])*count1, color='#FF4500', s=1.5)
+        ax1.text(equi-2.5, count1,str(label),horizontalalignment='right', verticalalignment='center')
+        print(count1, label)
+        count1 += 1
+for label in lig_inter.keys():
+    if sum(lig_inter[label][equi+1:]) > int((length-equi)/4):
+        ax2.scatter(list(range(equi+2, length+1)), np.array(lig_inter[label][equi+1:])*count2, color='b', s=1.5)
+        ax2.text(equi-2.5, count2,str(label),horizontalalignment='right', verticalalignment='center')
+        print(count2, label)
+        count2 += 1
+ax1.set_yticks([])
+ax2.set_yticks([])
+ax1.set_xlim(equi-10, length+1)
+ax2.set_xlim(equi-10, length+1)
+ax1.set_xticks([])
+plt.show()
+del t, key_inter, lig_inter
